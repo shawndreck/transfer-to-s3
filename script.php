d0b661341392b47f3f01664ef99da449b09ce6a5
Max Shawndreck <me@shawndreck.com>
2019-05-03 11:11:29 +0800

    Implement feature to exclude some files from uploads
    Notes:
    No need to upload zip files

--- script.php
+++ script.php
@@ -3,7 +3,6 @@
 require "./vendor/autoload.php";
 
 use Aws\S3\S3Client;
-use Aws\S3\Transfer;
 
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
@@ -22,11 +21,75 @@ $client = new S3Client([
 // Where the files will be source from
 $source = getenv('LOCAL_SOURCE_PATH');// '/path/to/source/files';
 
+function get_content_files($path)
+{
+    $path = realpath($path);
+    $files = array_diff(scandir($path), ['.', '..', '.DS_Store', '.gitignore', '.gitkeep', '.htaccess']);
+    $filtered = [];
+    foreach ($files as $file) {
+        if (strstr($file, '.zip')) {
+            continue;
+        }
+        $filtered[] = $path . DIRECTORY_SEPARATOR . $file;
+    }
+    return $filtered;
+}
+
+function get_relative_path($file)
+{
+    $marker = '/upload/';
+    return $relative_path = substr($file, strpos($file, $marker));
+}
+
+function is_file_exists_on_s3($file)
+{
+    global $client;
+    static $register = null;
+    if ($register === null) {
+        $client->registerStreamWrapper();
+        $register = true;
+    }
+    $relative_path = get_relative_path($file);
+    return file_exists('s3://' . getenv('AWS_BUCKET') . "/$relative_path");
+}
+
+function transfer_to_s3($files)
+{
+    $files = is_array($files) ? $files : [$files];
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            echo "$file is a directory\n";
+            $contents = get_content_files($file);
+            return transfer_to_s3($contents);
+        }
+
+        if (is_file_exists_on_s3($file)) {
+            continue;
+        }
+
+        $key = get_relative_path($file);
+        global $client;
+        $client->putObject([
+            'ACL'    => 'bucket-owner-full-control',
+            'Body'   => file_get_contents($file),
+            'Bucket' => getenv('AWS_BUCKET'),
+            'Key'    => $key,
+        ]);
+        echo "{$file} ==> s3://" . getenv('AWS_BUCKET') . "/{$key}" . PHP_EOL;
+    }
+    return true;
+}
+
+$base_path = "$source/upload";
+$files = get_content_files($base_path);
+transfer_to_s3($files);
+
+
 // Where the files will be transferred to
-$dest = 's3://' . getenv('AWS_BUCKET')  . '/upload';
+//$dest = 's3://' . getenv('AWS_BUCKET') . '/upload';
 
 // Create a transfer object
-$manager = new Transfer($client, $source, $dest);
+//$manager = new Transfer($client, $source, $dest);
 
 // Perform the transfer synchronously
-$manager->transfer();
+//$manager->transfer();
